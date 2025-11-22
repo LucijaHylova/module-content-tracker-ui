82eb6a49caad5f2e5a038c0e8b70acfb1e61981f
u234808 <lucia.hylova@sbb.ch>
2025-11-22 14:24:52 +0100

    first commit

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'dist/railway')));
+app.use(express.static(path.join(__dirname, 'dist/')));
 
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/railway/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(port, () => {

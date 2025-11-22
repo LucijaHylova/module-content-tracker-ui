37ce3cf70cdc6478ad61c7cb9f04eb7fd0fbf6d4
u234808 <lucia.hylova@sbb.ch>
2025-11-22 17:19:39 +0100

    first commit

--- server.js
+++ server.js
@@ -4,18 +4,19 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-// garantiert richtiger Pfad in Railway
-// const distFolder = path.resolve('dist/module-content-tracker-ui');
+// Absoluter Pfad zu deinem build (funktioniert lokal & Railway)
+const distFolder = path.join(process.cwd(), 'dist', 'module-content-tracker-ui', 'browser');
 
-app.use( express.static('dist/module-content-tracker-ui'));
-
-// app.get(/.*/, (req, res) => {
-//   res.sendFile(path.join('app/dist/module-content-tracker-ui', 'index.html'));
-// });
+// Static files ausliefern
+app.use(express.static(distFolder));
 
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.resolve('dist/module-content-tracker-ui', 'index.html'));
+  res.sendFile(path.join(path.join(distFolder, 'index.html')));
 });
+
+// app.get(/.*/, (req, res) => {
+//   res.sendFile(path.resolve('dist/module-content-tracker-ui', 'index.html'));
+// });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
